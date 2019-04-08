429d799fa606b05b435ed188a6f6e756f8ba7693
liarchASUS <liarchms@outlook.com>
2019-04-08 20:12:58 +0800

    每个人只操作自己的人物，只听自己的声音

---
+++ Assets/Script/Player_NetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_NetworkSetup : NetworkBehaviour
+{
+
+    [SeriallzeField] public Camera FPSCharacterCam;
+    [SeriallzeField] public AudioListener audioListener;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audioListener.enabled = true;
+        }
+    }
+}
